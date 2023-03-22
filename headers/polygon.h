a32db780ee5f9717cacdd2f3c26f1baf75b73893
Adele <chamouxadele@gmail.com>
2023-03-22 17:01:45 +0100

    adono

--- headers/polygon.h
+++ headers/polygon.h
@@ -10,5 +10,4 @@ Polygon *create_polygon(int n);
 void delete_polygon(Polygon * polygon);
 void print_polygon(Polygon * polygon);
 
-
 #endif
--- src/point.c
+++ src/point.c
@@ -1,6 +1,18 @@
+#include <stdio.h>
+#include "point.h"
 
-// create_point
+// create_point allows to dynamically allocate a structured Point whose coordinates are given in parameter
+Point *create_point(int px, int py){
+    Point p = {px, py};
+    return p;
+}
 
-// delete point
+// delete_point allows to free the memory allocated to the point given in parameter.
+void delete_point(Point * point){
+    free(Point);
+}
 
-// print_point
+// print_point allows to display on the screen the information of a Point in the following form: POINT [p(pos_x, pos_y)]
+void print_point(Point * p){
+    printf("POINT %d %d", pos_x, pos_y);
+}
