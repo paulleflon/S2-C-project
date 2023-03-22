3fc1314828c2f2e279a5a331c6de6a973e1e170a
Adele <chamouxadele@gmail.com>
2023-03-22 16:20:36 +0100

    square

---
+++ headers/point.h
@@ -0,0 +1,13 @@
+#ifndef POINT_H
+#define POINT_H
+
+typedef struct{
+    int pos_x;
+    int pos_y;
+}Point;
+
+Point *create_point(int px, int py);
+void delete_point(Point * point);
+void print_point(Point * p);
+
+#endif
---
+++ src/point.c
@@ -0,0 +1,6 @@
+
+// create_point
+
+// delete point
+
+// print_point
