9e6f1c130e999e039459800df91ccca15d6b14ba
hickatheworld <cc@hicka.world>
2023-03-22 17:06:34 +0100

    beginning of menu

--- main.c
+++ main.c
@@ -1,15 +1,31 @@
 #include <stdio.h>
+#include <string.h>
+
+int menu(char** options, int options_size) {
+	for (int i = 0; i < options_size; i++) {
+		printf("%c - %s\n", 65 + i, options[i]);
+	}
+	char choice;
+	do {
+		scanf("%c", &choice);
+	} while (
+		!(choice >= 65 && choice < 65 + options_size) && // Upper cased
+		!(choice >= 97 && choice < 97 + options_size) // Lower cased
+	);
+	return choice >= 97 ? choice - 97 : choice - 65;
+}
+
+void add_shape() {
+	printf("Select a shape:\n");
+	char* options = {"Point", "Line", "Circle", "Square", "Rectangle", "Polygon"};
+	int shape = menu(options, 6);
+}
+
 
 int main() {
-	printf("Hello, the projet modif");
-
-
-    printf("Please select an action: ");
-    printf("A- Add a shape");
-    printf("B- Display the list of shapes");
-    printf("C- Delete a shape");
-    printf("D- Drawing the shapes");
-    printf("E- Help");
-    printf("[Other actions]");
+	
+	printf("Select an option:\n");
+	char* options[] = {"Add a shape", "Display the list of shapes", "Delete a shape", "Draw shapes", "Help"};
+	int choice = menu(options, 5);
 	return 0;
 }
